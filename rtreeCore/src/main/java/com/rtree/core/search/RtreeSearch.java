0cd923fc1f631545db56d59cdd8d47501b46516d
dasong.wang <dasong.wang@ele.me>
2017-02-22 17:49:48 +0800

    add search point

--- rtreeCore/src/main/java/com/rtree/core/search/RtreeSearch.java
+++ rtreeCore/src/main/java/com/rtree/core/search/RtreeSearch.java
@@ -2,6 +2,7 @@ package com.rtree.core.search;
 
 import com.rtree.core.rtree.RTree;
 import com.rtree.core.rtree.geometry.Geometry;
+import com.rtree.core.rtree.geometry.Point;
 
 /**
  * Created by wangds on 17/2/14.
@@ -10,13 +11,12 @@ final class RtreeSearch {
 
     private static RTree<String, Geometry> tree = RTree.star().maxChildren(6).create();
     private static RtreeSearch instance = new RtreeSearch();
-// "{\"type\": \"FeatureCollection\", \"features\": [{\"geometry\": {\"type\": \"Polygon\", \"coordinates\": [[[121.417757, 31.025818], [121.446424, 31.034644], [121.450716, 31.023097], [121.421962, 31.015006], [121.417757, 31.025818]]]}, \"type\": \"Feature\", \"id\": \"aeac6eb6-84a5-11e5-8767-b82a72da8aa2\"}]}"
 
     private boolean addRtree() {
         return false;
     }
 
-    private boolean Search() {
+    private boolean Search(Point point) {
         return false;
     }
 
