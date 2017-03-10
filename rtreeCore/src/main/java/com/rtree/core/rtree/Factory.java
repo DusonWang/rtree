c9a8f170bbab148aab6973c739d740b6dfe596ba
dasong.wang <dasong.wang@ele.me>
2017-03-10 18:33:36 +0800

    commit

--- rtreeCore/src/main/java/com/rtree/core/rtree/Factory.java
+++ rtreeCore/src/main/java/com/rtree/core/rtree/Factory.java
@@ -4,5 +4,5 @@ package com.rtree.core.rtree;
 import com.rtree.core.rtree.geometry.Geometry;
 
 public interface Factory<T, S extends Geometry> extends LeafFactory<T, S>,
-    NonLeafFactory<T, S>, EntryFactory<T, S> {
+        NonLeafFactory<T, S>, EntryFactory<T, S> {
 }
